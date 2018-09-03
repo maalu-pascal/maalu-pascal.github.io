af3defd21050fea2bb2c0835013a666e5f7979d0
maalu-pascal <maalupascal@gmail.com>
2018-09-03 14:53:51 +0530

    Login page - accepts the username and password from the user and authenticates the user by comparing the input details with username and password stored in the json file login.json. When the user is authenticated, the page is redirected to the dashboard.

--- login/js/login.js
+++ login/js/login.js
@@ -1,20 +1,16 @@
-// localStorage.setItem("maalup","123456");
-var url = "../../home/html/home.html";
-
 function submitUser() {
-    var user = document.getElementById("username").value;
+    var userName = document.getElementById("username").value;
     var userPassword = document.getElementById("userpassword").value;
 
-    // if (localStorage.getItem(user) === userPassword) {
-    //     window.location = url;
-    // } else {
-    //     alert("Invalid Username/Password");
-    // }
-
-fetch('../json/login.json')
-    .then(res => res.json())
-    .then((out) => {
-        console.log('Output: ', out);
-}).catch(err => console.error(err));
+    fetch('../json/login.json')
+        .then(res => res.json())
+        .then((out) => {
+            console.log('Output: ', out[userName]);
+            if (out[userName] === userPassword) {
+                window.location.assign("../../home/html/home.html");
+            } else {
+                alert("Invalid Username/Password");
+            }
+        }).catch(err => console.error(err));
 
 }
