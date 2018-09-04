8b9b14dca19ca1bc47d3db964ac16879b766e40c
maalu-pascal <maalupascal@gmail.com>
2018-09-04 14:41:33 +0530

    Login page - The application is modified into a one page application. The user is authenticated and renders a welcome page if the user is authorized.

--- js/main.js
+++ js/main.js
@@ -1,39 +1,24 @@
-alert("home");
 window.onload = function (event) {
-    alert("home");
-    document.getElementById('login').style.display = 'none';
+    welcome();
 }
 
-function submitUser() {
-    var userName = document.getElementById("username").value;
-    var userPassword = document.getElementById("userpassword").value;
-
-    fetch('../json/login.json')
-        .then(res => res.json())
-        .then((out) => {
-            console.log('Output: ', out[userName]);
-            if (out[userName] === userPassword) {
-                window.location.assign("../../home/html/home.html");
-            } else {
-                alert("Invalid Username/Password");
-            }
-        }).catch(err => console.error(err));
-
-}
-
-
-function home() {
-
-    var login = document.getElementsByClassName('login');
-    document.getElementById('login').style.display = 'none';
-    document.getElementById('welcome').style.display = 'block';
-    login.style.display = none;
+function welcome() {
+    req = new XMLHttpRequest();
+    req.open("GET", "../welcome/html/welcome.html", false);
+    req.send(null);
+    document.getElementById("content").innerHTML = req.responseText;
 }
 
 function login() {
-    var login = document.getElementById('welcome');
-    login.style.display = 'none';
-    document.getElementById('login').style.display = 'block';
-    
+    req = new XMLHttpRequest();
+    req.open("GET", "../login/html/login.html", false);
+    req.send(null);
+    document.getElementById("content").innerHTML = req.responseText;
 }
 
+function home() {
+    req = new XMLHttpRequest();
+    req.open("GET", "../home/html/home.html", false);
+    req.send(null);
+    document.getElementById("content").innerHTML = req.responseText;
+}
--- login/js/login.js
+++ login/js/login.js
@@ -1,15 +1,13 @@
 function submitUser() {
-    alert("login");
     var userName = document.getElementById("username").value;
     var userPassword = document.getElementById("userpassword").value;
 
-    fetch('../json/login.json')
+    fetch('../login/json/login.json')
         .then(res => res.json())
         .then((out) => {
             console.log('Output: ', out[userName]);
             if (out[userName] === userPassword) {
-                alert("Invalid Username/Password");
-                window.location.assign("../../home/html/home.html");
+                home();
             } else {
                 alert("Invalid Username/Password");
             }
