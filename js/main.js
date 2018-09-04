1ca8f98a13a3a82b6eb348c6adc00ba9f862f2fe
maalu-pascal <maalupascal@gmail.com>
2018-09-04 16:03:45 +0530

    Dead code removed.

--- js/main.js
+++ js/main.js
@@ -2,20 +2,6 @@ window.onload = function (event) {
     containerContent('../welcome/html/welcome.html');
 }
 
-function login() {
-    req = new XMLHttpRequest();
-    req.open("GET", "../login/html/login.html", false);
-    req.send(null);
-    document.getElementById("content").innerHTML = req.responseText;
-}
-
-function home() {
-    req = new XMLHttpRequest();
-    req.open("GET", "../home/html/home.html", false);
-    req.send(null);
-    document.getElementById("content").innerHTML = req.responseText;
-}
-
 function containerContent(url) {
     req = new XMLHttpRequest();
     req.open("GET", url, false);
