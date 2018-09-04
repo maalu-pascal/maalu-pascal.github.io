2f42944018287004c917cc28d02e0b67ec242a89
maalu-pascal <maalupascal@gmail.com>
2018-09-04 19:26:15 +0530

    JSON file edited. Corresponding logic applied to authorize the user during login.

--- login/js/login.js
+++ login/js/login.js
@@ -1,14 +1,20 @@
 function submitUser() {
     var userName = document.getElementById("username").value;
     var userPassword = document.getElementById("userpassword").value;
-
+    var userFound = 0;
     fetch('../login/json/login.json')
         .then(res => res.json())
         .then((out) => {
-            console.log('Output: ', out[userName]);
-            if (out[userName] === userPassword) {
-                containerContent('../home/html/home.html');
-            } else {
+            out.users.forEach(element => {
+                console.log(element.username);
+                if ((element.username === userName) && (element.password === userPassword)) {
+                    userFound = 1;
+                    containerContent('../home/html/home.html');
+
+                }
+            }
+            );
+            if (!userFound) {
                 alert("Invalid Username/Password");
             }
         }).catch(err => console.error(err));
