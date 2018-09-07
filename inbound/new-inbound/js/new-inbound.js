b471e286d4d6ac5493b5e0aaadf743a220915979
maalu-pascal <maalupascal@gmail.com>
2018-09-07 18:28:43 +0530

    Basi routing for create-new-inbound created.

---
+++ inbound/new-inbound/js/new-inbound.js
@@ -0,0 +1,8 @@
+function newInbound() {
+        containerContent('../inbound/new-inbound/html/new-inbound.html');
+        category();
+}
+
+function category() {
+    var category = document.getElementById("selectCategory");
+}
