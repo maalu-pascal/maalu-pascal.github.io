9ee92c781c02619e181d3ae7ff126ead86e8fd7e
maalu-pascal <maalupascal@gmail.com>
2018-09-12 14:18:09 +0530

    The inbound list is read from the localStorage. The name and date is displayed in table format.

--- inbound/inbound-list/js/inbound-list.js
+++ inbound/inbound-list/js/inbound-list.js
@@ -1,5 +1,30 @@
 function inbound() {
     containerContent('../inbound/inbound-list/html/inbound-list.html');
-    
+    inboundList();
+}
+
+function inboundList() {
+    var inbound = localStorage.getItem("inbound");
+    inbound = JSON.parse(inbound);
+
+    var newRow = document.getElementById("tableBody");
+
+    for (eachInbound in inbound) {
+
+        var row = document.createElement("tr");
+        row.setAttribute("onclick","inboundProductList('"+inbound[eachInbound].name+"')");
+        
+        var nameData = document.createElement("td");
+        var name = document.createTextNode(inbound[eachInbound].name);
+        nameData.appendChild(name);
+        row.appendChild(nameData);
+
+        var dateData = document.createElement("td");
+        var date = document.createTextNode(inbound[eachInbound].date);
+        dateData.appendChild(date);
+        row.appendChild(dateData);
+
+        newRow.appendChild(row);
+    }
 }
 
