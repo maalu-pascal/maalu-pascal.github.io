a4ced3b7f6912dfcc4c8cdf3683e12c57d7984a7
maalu-pascal <maalupascal@gmail.com>
2018-09-17 20:08:08 +0530

    Inventory-list moved to the shared folder.

---
+++ shared/js/inventory-list.js
@@ -0,0 +1,24 @@
+function inboundOutboundList(listName) {
+    var inventoryList = localStorage.getItem(listName);
+    inventoryList = JSON.parse(inventoryList);
+
+    var newRow = document.getElementById("tableBody");
+
+    for (eachInventory in inventoryList) {
+
+        var row = document.createElement("tr");
+        row.setAttribute("onclick", "inventoryProductList('" + inventoryList[eachInventory].name + "','" + listName + "')");
+
+        var nameData = document.createElement("td");
+        var name = document.createTextNode(inventoryList[eachInventory].name);
+        nameData.appendChild(name);
+        row.appendChild(nameData);
+
+        var dateData = document.createElement("td");
+        var date = document.createTextNode(inventoryList[eachInventory].date);
+        dateData.appendChild(date);
+        row.appendChild(dateData);
+
+        newRow.appendChild(row);
+    }
+}
