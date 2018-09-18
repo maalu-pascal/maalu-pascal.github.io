286599bc36dca4f44e91dd7ab76544d6a8543892
maalu-pascal <maalupascal@gmail.com>
2018-09-18 18:53:56 +0530

    The date is convert to dateString before displaying.

--- shared/js/inventory-list.js
+++ shared/js/inventory-list.js
@@ -5,9 +5,11 @@ function inboundOutboundList(listName) {
     var newRow = document.getElementById("tableBody");
 
     for (eachInventory in inventoryList) {
+        var inventoryDate = new Date(inventoryList[eachInventory].date);
+            inventoryDate = inventoryDate.toDateString();
         var row = `<tr onclick= "inventoryProductList('${inventoryList[eachInventory].name}','${inventoryList[eachInventory].date}','${listName}')">
                    <td>${inventoryList[eachInventory].name}</td>
-                   <td>${inventoryList[eachInventory].date}</td> </tr>`;
+                   <td>${inventoryDate}</td> </tr>`;
         newRow.insertAdjacentHTML("beforeend", row);
     }
 }
