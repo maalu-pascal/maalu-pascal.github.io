18b63cb4f55601491717a473b75e9369e7921101
maalu-pascal <maalupascal@gmail.com>
2018-10-04 11:40:47 +0530

    Routing modified. Scripts are loaded dynamically.

--- shared/js/route.js
+++ shared/js/route.js
@@ -1,71 +1,81 @@
 const routes = [
     {
-        name: 'welcome',
-        path: 'welcome/html/welcome.html',
+        html: 'welcome/html/welcome.html',
         javascriptFunction: '',
+        name: 'welcome',
+        script: '.',
         style: '<link rel="stylesheet" type="text/css" media="screen" href="welcome/css/welcome.css" />',
         url: 'welcome'
     },
     {
-        name: 'login',
-        path: 'login/html/login.html',
-        style: '<link rel="stylesheet" type="text/css" media="screen" href="login/css/login.css" />',
+        html: 'login/html/login.html',
         javascriptFunction: 'checkSubmit',
+        name: 'login',
+        script: 'login/js/login.js',
+        style: '<link rel="stylesheet" type="text/css" media="screen" href="login/css/login.css" />',
         url: 'login'
     },
     {
-        name: 'dashboard',
-        path: 'dashboard/html/dashboard.html',
+        html: 'dashboard/html/dashboard.html',
         javascriptFunction: 'loadChart',
+        name: 'dashboard',
+        script: 'dashboard/js/dashboard.js',
         style: '<link rel="stylesheet" type="text/css" media="screen" href="dashboard/css/dashboard.css" />',
         url: 'dashboard'
     },
     {
-        name: 'current-stock',
-        path: 'current-stock/html/current-stock.html',
+        html: 'current-stock/html/current-stock.html',
         javascriptFunction: 'displayCurrentStock',
+        name: 'current-stock',
+        script: 'current-stock/js/current-stock.js',
         style: '<link rel="stylesheet" type="text/css" media="screen" href="current-stock/css/current-stock.css" />',
         url: 'current-stock'
     },
     {
-        name: 'inbound',
-        path: 'inbound/inbound-list/html/inbound-list.html',
+        html: 'inbound/inbound-list/html/inbound-list.html',
         javascriptFunction: 'inventory',
+        name: 'inbound',
+        script: 'shared/js/inventory-list.js',
         style: '<link rel="stylesheet" type="text/css" media="screen" href="shared/css/inventory-list.css" />',
         url: 'inbound'
     },
     {
-        name: 'new-inbound',
-        path: 'inbound/new-inbound/html/new-inbound.html',
+        html: 'inbound/new-inbound/html/new-inbound.html',
         javascriptFunction: 'createNewInventory',
+        name: 'new-inbound',
+        script: 'shared/js/new-inventory.js',
         style: '<link rel="stylesheet" type="text/css" media="screen" href="shared/css/new-inventory.css" />',
         url: 'inbound/new-inbound'
     },
     {
-        name: 'inbound-product-list',
-        path: 'inbound/inbound-product-list/html/inbound-product-list.html',
+        html: 'inbound/inbound-product-list/html/inbound-product-list.html',
         javascriptFunction: 'productList',
+        name: 'inbound-product-list',
+        script: 'shared/js/inventory-product-list.js',
         style: '<link rel="stylesheet" type="text/css" media="screen" href="shared/css/inventory-product-list.css" />',
         url: 'inbound/inbound-product-list'
     },
     {
-        name: 'outbound',
-        path: 'outbound/outbound-list/html/outbound-list.html',
+        html: 'outbound/outbound-list/html/outbound-list.html',
         javascriptFunction: 'inventory',
+        name: 'outbound',
+        script: 'shared/js/inventory-list.js',
         style: '<link rel="stylesheet" type="text/css" media="screen" href="shared/css/inventory-list.css" />',
         url: 'outbound'
     },
     {
-        name: 'new-outbound',
-        path: 'outbound/new-outbound/html/new-outbound.html',
+        html: 'outbound/new-outbound/html/new-outbound.html',
         javascriptFunction: 'createNewInventory',
+        name: 'new-outbound',
+        script: 'shared/js/new-inventory.js',
         style: '<link rel="stylesheet" type="text/css" media="screen" href="shared/css/new-inventory.css" />',
         url: 'outbound/new-outbound'
     },
     {
-        name: 'outbound-product-list',
-        path: 'outbound/outbound-product-list/html/outbound-product-list.html',
+        html: 'outbound/outbound-product-list/html/outbound-product-list.html',
         javascriptFunction: 'productList',
+        name: 'outbound-product-list',
+        script: 'shared/js/inventory-product-list.js',
         style: '<link rel="stylesheet" type="text/css" media="screen" href="shared/css/inventory-product-list.css" />',
         url: 'outbound/outbound-product-list'
     }
@@ -81,7 +91,7 @@ const routes = [
  */
 function redirectTo(name, inventoryType, inventoryName, inventoryDate) {
     //Route details are read.
-    var route = routes.filter(function (r) { return r.name === name })[0];
+    var route = routes.filter(function (newRoute) { return newRoute.name === name })[0];
 
     //url is changed.
     window.history.pushState(route.name, `state${route.name}`, `#${route.url}`);
@@ -90,8 +100,17 @@ function redirectTo(name, inventoryType, inventoryName, inventoryDate) {
     document.getElementById("styles").innerHTML = route.style;
 
     //The corresponding html section is rendered.
-    containerContent(route.path);
-    
-    //inventoryName, inventoryDate is only for redirectTo('inventory-product-list').
-    if (route.javascriptFunction) window[route.javascriptFunction](inventoryType, inventoryName, inventoryDate);    
+    containerContent(route.html);
+
+    //The corresponding script file is loaded.
+    let parent = document.getElementById("JSscripts");
+    let childScript = document.createElement("script");
+    childScript.src = route.script;
+    childScript.onload = function () {
+        //inventoryName, inventoryDate is only for the function redirectTo('inventory-product-list').
+        if (route.javascriptFunction) window[route.javascriptFunction](inventoryType, inventoryName, inventoryDate);
+    }
+    parent.innerHTML = '';
+    parent.appendChild(childScript);
 }
+
